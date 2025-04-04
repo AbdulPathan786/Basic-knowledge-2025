56ee2efedf602bdd8d90225d803129f1414cad02
Abdul Pathan <103238912+AbdulPathan786@users.noreply.github.com>
2025-04-04 12:24:23 +0530

    Server command

---
+++ commond.java
@@ -0,0 +1,59 @@
+https://api.indicrm.io/ibs/api/whatsapp/webhook
+
+filZilla
+	ssh root@103.186.185.93
+	%bmZfBfNRj4H4
+
+103.186.185.93
+
+Server
+	103.186.185.93
+	indicrm123
+
+	https://api.indicrm.io
+
+-------frontend------------
+constant.js file change url 
+--VScode--
+.\run.bat
+git add .
+git commit -m "updates changes"
+git push origin main
+
+--CMD
+git pull
+npm run build
+
+
+-------backend------------
+db file change
+
+.\run.bat
+git add .
+git commit -m "updates changes"
+git push origin main
+
+ssh root@103.186.185.93
+	%bmZfBfNRj4H4
+	
+root@vps:~# cd /home/
+root@vps:/home# cd ibs-whatsapp-backend/
+root@vps:/home/ibs-whatsapp-backend# git pull
+root@vps:/home/ibs-whatsapp-backend# pm2 restart 1
+root@vps:/home/ibs-whatsapp-backend# pm2 stop 0
+
+---------------------
+http://103.186.185.93/ibs/api/whatsapp_webhook/contacts
+
+
+
+
+/var/www/html
+
+
+
+
+
+
+
+
